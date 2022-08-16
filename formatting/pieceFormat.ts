4f51412ae7ae07817806fd27b41799a6c62a1eff
Suyooo <me@suyo.be>
2022-08-16 23:40:11 +0200

    Base pieceFormat on Language

--- formatting/pieceFormat.ts
+++ formatting/pieceFormat.ts
@@ -1,22 +1,40 @@
 import PieceInfo from "../types/pieceInfo";
+import Language from "../types/language";
+import Attribute from "../types/attribute";
 
-export function pieceFormat(pieces: PieceInfo): string {
+export function pieceFormat(pieces: PieceInfo, lang: Language): string {
     let s = "";
     let comma = "";
     if (pieces.all > 0) {
-        s += "<span class='pieceno'><span class='piece all'>[ALL] x</span><span>" + pieces.all + "</span></span>";
+        s += "<span class='pieceno'><span class='piece all'>"
+            + lang.leftSquareBracket
+            + Attribute.get("ALL")[lang.pieceNameProperty]
+            + lang.rightSquareBracket + lang.times
+            +"</span><span>" + pieces.all + "</span></span>";
         comma = ", ";
     }
     if (pieces.smile > 0) {
-        s += "<span class='pieceno'><span class='piece smile'>" + comma + "[SMILE] x</span><span>" + pieces.smile + "</span></span>";
+        s += "<span class='pieceno'><span class='piece smile'>"
+            + comma + lang.leftSquareBracket
+            + Attribute.get("SMILE")[lang.pieceNameProperty]
+            + lang.rightSquareBracket + lang.times
+            + "</span><span>" + pieces.smile + "</span></span>";
         comma = ", ";
     }
     if (pieces.pure > 0) {
-        s += "<span class='pieceno'><span class='piece pure'>" + comma + "[PURE] x</span><span>" + pieces.pure + "</span></span>";
+        s += "<span class='pieceno'><span class='piece pure'>"
+            + comma + lang.leftSquareBracket
+            + Attribute.get("PURE")[lang.pieceNameProperty]
+            + lang.rightSquareBracket + lang.times
+            + "</span><span>" + pieces.pure + "</span></span>";
         comma = ", ";
     }
     if (pieces.cool > 0) {
-        s += "<span class='pieceno'><span class='piece cool'>" + comma + "[COOL] x</span><span>" + pieces.cool + "</span></span>";
+        s += "<span class='pieceno'><span class='piece cool'>"
+            + comma + lang.leftSquareBracket
+            + Attribute.get("COOL")[lang.pieceNameProperty]
+            + lang.rightSquareBracket + lang.times
+            + "</span><span>" + pieces.cool + "</span></span>";
     }
     return s;
 }
