a443af117d0becbe5cdfb0b49a22b980ddd84c10
Suyooo <me@suyo.be>
2022-08-25 00:15:36 +0200

    Translation update: "next player" -> "next player in turn order"

--- utils/gttofaqtext.ts
+++ utils/gttofaqtext.ts
@@ -2,112 +2,107 @@ import fs from "fs";
 import Card from "../models/card/card";
 import DB from "../models/db";
 
-const faqname = "LL06";
-let tlresult = `- LL06-028 SR [Chika Takami] ~ LL06-036 SR [Ruby Kurosawa]
+const faqname = "LL07";
 
-●Who are the “members on stage”?
--LL06-046 HR [Chika Takami]
+if (fs.existsSync("server/frontend/views/faq/" + faqname.toLowerCase() + ".ejs")) {
+    console.log("File already exists - not overwriting");
+    process.exit(0);
+}
 
-Q. When this card was 《Enter》, I only had one card in my hand. I want to have 0 cards in my hand, so is it possible to put only 1 card on the bottom of the deck?
-A. No. You cannot put only one card on the bottom of your deck.
-
-Q. This card 《Enter》 in a situation where there is no card in the deck. Can this skill be used to 《Enter》 a card placed in the deck?
-A. yes. Of the two cards placed in the deck from the hand, the upper card will be "appeared" as it is.
-●LL06-047 HR [Riko Sakurauchi]
-
-●Who are the “members on stage”?
-
-Q. What does "There are two or more face-up music cards" mean?
-A. Count the types of music card colors (red, green, blue, yellow) in your set list, and if there are two or more colors, the condition is met.
-For example, if there are EX03-028 "Is your heart shining?", LL04-064 "Aozora Jumping Heart", and LL06-058 "Jingle bells don't stop", the breakdown of colors is "green, yellow, yellow". , there are two colors.
-●LL06-048 HR [Kanan Matsuura]
-
-Q. What happens if I Enter when I have 2 or less cards in my deck?
-A. If you have 2 cards in your deck, show 2 cards, add one to your hand, and place the other on the bottom of your deck.
-If you put "Kanan" on the bottom, you can 《Enter》 "Kanan" with 【LIVE】 from your hand.
-If there is only one card in your deck, you can show it and add it to your hand, but since you can't put a card on the bottom of your deck, "Kanan" with [LIVE] can't 《Appear》 from your hand. (You cannot choose not to add one card to your hand.)
-If the deck is 0 cards, nothing happens.
--LL06-049 HR [Dia Kurosawa]
-
-Q. Does "Appear from hand" include "Appear" by [RUSH] and "Appear" by skills such as LL04-048 "Kanan Matsuura"?
-A. yes. This includes not only "Appearance" as an action performed on the turn, but also "RUSH" and various skills.
-- LL06-050 HR [Watanabe You]
+let tlresult = `- LL07-037 SR [Chika Takami] ~ LL07-045 SR [Ruby Kurosawa]
 
 ● What is the status of "Waiting"?
+- LL07-046 HR [Chika Takami]
 
-●Is there an order to do things within the same skill?
+Q. What happens if I Enter when I have 2 or less cards in my deck?
+A. Even if there are 2 or less cards in the deck, add 1 member with [☆] from among them to your hand, and place the remaining cards on the bottom of the deck. If all the members you showed were members without a [☆], you cannot take another turn because you did not meet the condition of "showing three members without a [☆]".
+●LL07-047 HR [Riko Sakurauchi]
 
-Q. What do you mean by "If there are 3 or more costumes for the waiting 'You'?"
-A. Count the types of "live costumes" on the bottom left of the member card.
-If there are members of "Aozora Jumping Heart", "Aozora Jumping Heart", "Kimi no Kokoro wa Kagayaiterukai?", "Kimi no Kokoro wa Kagayaterukai?" There are different kinds.
+Q. If there is only one face-up song card in the setlist, can a skill turn one face-down card face-up?
+A. yes. Since it satisfies the condition that "(the face-up music cards in the setlist are all the same color)", you can turn one face-down card face-up.
+●LL07-048 HR [Kanan Matsuura]
 
-Q. When performing a Live with this card's skill, do I have to participate in the live costume "Week" that I referred to?
-A. No. Participation is not required.
+Q. What exactly do you mean by "having exactly the same piece"?
+A. It means that "each member has the same type and number of pieces". The order of the pieces in the card does not matter, so you can "appear" members with [Smile] and [pure] skills, and [appear] members with [pure] and [smile] skills.
 
-Q. Not all of the waiting members are "Week", but there are 3 or more types of live costumes for "Week".
-In this case, can I 《Live》 with this skill?
-A. No. If the first condition is not satisfied, nothing will be done after "further ~".
-●LL06-051 HR [Yoshiko Tsushima]
+Q. If I 《Enter》 LL06-008 “Mari Ohara” who has a birthday bonus with this card’s skill, can I 《Enter》 LL07-053 “Yoshiko Tsushima” from my hand?
+A. yes. When the birthday bonus is applied, the bonus pieces are always added, so in the example, LL07-053 "Yoshiko Tsushima" with the [Cool] and [All] pieces appears. I can do it.
+-LL07-049 HR [Dia Kurosawa]
 
-● What if a member has more than one skill?
+Q. Can I choose not to draw a card even if "Everyone draws a card" is chosen?
+A. No. If "Everyone draws a card" is chosen, you and everyone will always draw a card.
+- LL07-050 HR [You Watanabe]
+
+Q. If I 《Enter》 when I have 0 cards in my deck, will the card I put on the bottom of my deck be 《Enter》 as it is?
+A. yes. Look at the card placed at the bottom of the deck and "appear" one of the members, so the card you put will always "appear".
+-LL07-052 HR [Hanamaru Kunikida]
+
+●Is it OK if the condition is the same as "If more than"?
+● [When participating in the live] [When the live is successful] What is the order of skills?
+Q. Two people are playing a game and both have the same number of cards in their hand. In this situation, what happens if two or more of these cards participate in the same 《Live》?
+A. The skill of the first player, Hanamaru, draws a card. The second and subsequent "Hanamaru" meet the condition "You have more cards in your hand than any other" at the time of skill, so + [all].
+●LL07-053 HR [Mari Ohara]
+
+Q. Can a member who does not have [All], but has a skill that + [All] such as [When participating in a live], use this card's skill to [Appear]?
+A. No. Skills such as [When participating in a live] can only be used at the appropriate timing, so when you are in your hand, you are not a member with [All].
+
+Q. [When Appearing] A member who has [Special Practice] with the skill is [appeared], and by becoming awakened, the number of [All] pieces has increased to 3 or more. In this case, can you draw 3 cards?
+A. yes. This [When entering] skill counts the number of pieces after members appear on the stage. [Special Practice] If you are awakened by a skill, the awakening icon piece will be active when you appear on the stage, so the member will have 3 [All] pieces, including the awakening icon piece. If you have more than that, you meet the conditions.
+-LL07-055 M [near future happy ending]
+
+● What is the status of "Waiting"?
+●Which comes first, opening the song or the [Successful Live] skill?
+Q. At the time of "live" this song, I achieved the goal live P. If the conditions for the skill are met, can I continue to 《Live》?
+A. No. Once the target live P is achieved, the winner will be eliminated, so "live" is not possible.
+
+Q. What would happen if members other than "CYaRon!" participated in the "live" of this song?
+A. If "Chika", "You", and "Ruby" are participating, the skill will be effective regardless of the other members.
+-LL07-056M [GALAXY HidE and SeeK]
+
+Q. LL05-053 "Mari Ohara" is playing this song while meeting the skill conditions and performing live, with members with [Smile], members with [Pure], and members with [Cool]. "I participated in. In this case, will the skill be +【All】?
+A. No. The skill of this song changes all the [Smile], [Pure], and [Cool] pieces of the participating members to [All] before the [When participating in a live] skill. Therefore, in this case, +[all] is not performed.
+
+Q. The member who participated in the "live" was given a + [smile] by the skill "When participating in the live" while the song was being played live with the skill conditions met. Does this piece also become [All] by skill?
+A. yes. A piece becomes [all] when it is added.
+
+Q. What will happen if members other than "AZALEA" participate in the "live" of this song?
+A. As long as "Kanan", "Dia" and "Hanamaru" are participating, the skill will be effective regardless of the other members.
+- LL07-057 M [Kowareyasuki]
+
+● [When participating in the live] [When the live is successful] What is the order of skills?
+●Which comes first, opening the song or the [Successful Live] skill?
+Q. What exactly do you mean when you say "each person may appear"?
+A. It means that "up to one "Riko", one "Yoshiko", and one "Mari" may appear". It is possible to 《Appear》 three people one by one, or to 《Appear》 one by one “Riko” and “Yoshiko”.
+
+Q. What will happen if members other than "Guilty Kiss" participate in the "live" of this song?
+A. If "Riko", "Yoshiko", and "Mari" are participating, the skill will be effective regardless of the other members.
+●LL07-058M “Landing action Yeah!!”
+
+●Who are the “members on stage”?
+- LL07-059 M [Cinderella on the G line]
+
+Q. If there are multiple pieces of this song in the live, will the target live P be added for each piece?
+A. yes. It will be +1 for each song of this live.
+
+Q. When the target live P was 9 points, this song was "live" and the live P became 9 points. Will this be a winner?
+A. No. Since the target live P was +1, we need 1 more point.
+
+Q. In a situation where there are 3 or more people playing and there is a player who has already won with 9 points, what will happen if this song is played "live"?
+A. Players who have already won will remain victorious. The target live P of the remaining players is +1.
+
+Q. The player who played this song "live" won. [During Live] Will the skill continue after that?
+A. yes. The [During Live] skill continues until the end of this game.
+- LL07-062 M [Thoughts become one]
+
+●Who are the “members on stage”?
+●How should I count the number of members?
+●LL07-063M "HAPPY PARTY TRAIN"
 
 ●How should I count the number of members?
+- LL07-064 SP [Chika Takami] ~ LL07-081 SEC [Ruby Kurosawa]
 
-Q. What do you mean by "If you weren't resting your turn in this game" or "If you weren't resting your turn"?
-A. Mainly by card skills (example: LL01-071 "No brand girls" / "START: DASH!!", LL04-051 "Yoshiko Tsushima")
-"Rest one (or more) of your turns. ] more than once,
-It will be "I was resting my turn in this game". Conversely, if you didn't go, it means you didn't rest your turn.
-
-Q. Can I 'rest my turn' by intentionally not performing an action on my turn?
-A. No. Please do either "appearance", "invitation" or "live".
--LL06-052 HR [Hanamaru Kunikida]
-
-● [When participating in the live] [When the live is successful] What is the order of skills?
-
-Q. If 2 or more of this card participate in the same 《Live》, will all of those pieces become 【All】?
-A. yes. Each makes the 【Smile】 of the other "Hanamaru" into 【All】, so in the end all the "Hanamaru" pieces become 【All】.
-●LL06-053 HR [Mari Ohara]
-
-● What if a member has more than one skill?
-
-● What if the top card of the deck is turned face up?
-
-● [When participating in the live] [When the live is successful] What is the order of skills?
--LL06-054 HR [Ruby Kurosawa]
-
-Q. Does "Appear from the deck" include "Appear" by skills such as LL04-054 "Ruby Kurosawa"?
-A. yes. This includes not only "Appearance" as an action performed on the turn, but also by skill.
-
-Q. If I use Ruby's skill to 《Enter》 from my deck, can I still 《Enter》 the card I drew first?
-A. yes. You can 《appear》.
--LL06-058 M [Jingle Bells Can't Stop]
-
-●Which comes first, opening the song or the [Successful Live] skill?
-
-Q. What does "Member with the fewest member cards participating in this [Live]" mean?
-A. Count the number of participating member cards for each member with a different name participating in this "live".
-For example, if "Chika" has 3 cards, "You" has 2 cards, and "Riko" has 1 card, "Riko" will be the member with the fewest participating member cards.
-
-Q. Can I choose 0 members for "Member with the fewest participating member cards"?
-A. No. You cannot choose a member who has not participated in even one card.
-
-Q. If there are multiple "members with the fewest participating member cards", can multiple members with different names be selected and "appeared"?
-A. No. Please choose one member from among the fewest members. Other members cannot 《Appear》.
--LL06-059 M [Holy Day Prayer]
-
-Q. In a two-player game, can the second player use skills?
-A. yes. Regardless of how many people are participating in the game, the condition is satisfied if the order to take the turn is the last.
-
-Q. The game was played by 3 people, and the 3rd player won. After that, can the 2nd player use the skill?
-A. No. We only look at the order decided in the game preparation, so the last player will not be moved up by winning.
-● LL06-061 M “Because the sky and heart will be clear”
-
-● What if you use a skill to turn face-up cards in the set list face-down?
-
-● [When participating in the live] [When the live is successful] What is the order of skills?
-● LL06-062 M "Waku-Waku-Week!"
-
-● [When participating in the live] [When the live is successful] What is the order of skills?`;
+Q. [At the start] If multiple players have the starting members with skills face up, in what order should the skills be used?
+A. Skills are used in order to advance the turn. Once everyone has used their skills, the player taking the first turn starts their turn.`;
 
 tlresult = tlresult.replace(/^-/gm, "●");
 tlresult = tlresult.replace(/^● /gm, "●");
@@ -186,5 +181,5 @@ let faqresult = `<%- include("../partials/header", {"title": "${faqname} FAQ &bu
 </div>
 <%- include("../partials/footer", {"jquery": false, "scripts": []}); %>`;
 
-    fs.writeFileSync("frontend/views/faq/" + faqname.toLowerCase() + ".ejs", faqresult);
+    fs.writeFileSync("server/frontend/views/faq/" + faqname.toLowerCase() + ".ejs", faqresult);
 })();
