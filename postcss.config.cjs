59279e43319e7fe247d78a3de524eff1e3725df9
Suyooo <me@suyo.be>
2023-01-06 20:18:40 +0100

    Set up styling

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,9 @@
+const tailwind = require("tailwindcss");
+const tailwindNested = require("tailwindcss/nesting");
+const tailwindConfig = require("./tailwind.config.cjs");
+const autoprefixer = require("autoprefixer");
+const postcssNesting = require("postcss-nesting");
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [ tailwindNested(postcssNesting), tailwind(tailwindConfig), autoprefixer]
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: colors.blue[900],
+        primary: colors.blue,
+        accent: colors.orange,
+        attribute: {
+          all: "#ffd87f",
+          smile: "#fa7dca",
+          pure: "#8ec4a6",
+          cool: "#a2e3ff"
+        }
+      },
+      borderRadius: {
+        "4xl": "2rem"
+      }
+    }
   },
   plugins: [],
 }
