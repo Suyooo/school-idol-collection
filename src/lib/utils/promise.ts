d1711eab232977efebfac36f893f71f409103b31
Suyooo <me@suyo.be>
2023-05-12 18:56:50 +0200

    Add delay to retryPromise to fix hang on refresh

--- src/lib/utils/promise.ts
+++ src/lib/utils/promise.ts
@@ -1,5 +1,5 @@
 export function retryPromise<T>(p: Promise<T>): Promise<T> {
     return new Promise<T>((resolve) => {
-        p.then(res => resolve(res)).catch(() => resolve(retryPromise(p)));
+        p.then(res => resolve(res)).catch(() => new Promise(resolve => setTimeout(() => resolve(retryPromise(p)), 100)));
     });
 }
