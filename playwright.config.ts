0290d09878693f160e4bbbf10cda69cd88727491
Suyooo <me@suyo.be>
2023-11-01 16:45:06 +0100

    Disable playwright tracing

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,11 @@ export default defineConfig({
     testDir: "./tests",
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
+    retries: 2,
     workers: process.env.CI ? 1 : undefined,
     use: {
         baseURL: "http://127.0.0.1:5173",
-        trace: "on-first-retry",
+        trace: "off",
     },
 
     projects: [
