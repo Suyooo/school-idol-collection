8faac487567e9d01f3abb06e748056c3f3cc4908
Suyooo <me@suyo.be>
2023-11-04 17:16:09 +0100

    Card added: PR-271

--- src/routes/(site)/+page.server.ts
+++ src/routes/(site)/+page.server.ts
@@ -1,7 +1,7 @@
 import type Card from "$models/card/card.js";
 import type { PageServerLoad } from "./$types.js";
 
-const latestCardNo = "PR-270";
+const latestCardNo = "PR-271";
 const latestSetId = "LL17";
 const latestSetCardNos = [
     [10, 9],
