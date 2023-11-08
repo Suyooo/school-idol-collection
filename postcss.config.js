3e6120862dd7d970019b1e2db5834d70851b9a7f
Suyooo <me@suyo.be>
2023-11-08 12:43:15 +0100

    Make tailwind config hot reload work

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-import autoprefixer from "autoprefixer";
-import postcssNesting from "postcss-nesting";
-import tailwind from "tailwindcss";
-import tailwindNested from "tailwindcss/nesting/index.js";
-import tailwindConfig from "./tailwind.config.js";
-
-export default {
-	plugins: [tailwindNested(postcssNesting), tailwind(tailwindConfig), autoprefixer],
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+import tailwindNested from "tailwindcss/nesting/index.js";
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindConfig from "./tailwind.config.js";
 
 export default defineConfig(() => {
 	return {
 		plugins: [sveltekit()],
+		css: {
+			postcss: {
+				plugins: [tailwindNested, tailwind(tailwindConfig), autoprefixer],
+			},
+		},
 	};
 });
