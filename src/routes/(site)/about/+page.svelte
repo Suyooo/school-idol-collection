ae74f06a5b65e0c91cc1cadde6b7a763de94d968
Suyooo <me@suyo.be>
2023-11-15 15:33:03 +0100

    About restyled

--- src/routes/(site)/about/+page.svelte
+++ src/routes/(site)/about/+page.svelte
@@ -2,42 +2,43 @@
 	<title>About &bull; SIC</title>
 </svelte:head>
 
-<div class="content">
-	<h3>About</h3>
-	<div class="panel">
-		<div class="panel-inner">
-			<div class="mt-2">
-				This site and all the translations were made by Suyooo.<br />
-				For questions/problems/mistakes, contact me here:
-			</div>
-			<ul class="list-disc list-inside mt-2 ml-4">
-				<li>Mastodon: <a href="https://mstdn.schoolidol.club/@Suyooo">@Suyooo@mstdn.schoolidol.club</a></li>
-				<li>Tumblr: <a href="https://suyooo.tumblr.com/">Suyooo</a></li>
-				<li>
-					Or <a href="https://github.com/Suyooo/school-idol-collection/issues">open an Issue on GitHub</a>
-				</li>
-			</ul>
-			<div class="mt-4">Some cool stuff used for this site:</div>
-			<ul class="list-disc list-inside mt-2 ml-4">
-				<li><a href="https://kit.svelte.dev/">SvelteKit</a></li>
-				<li><a href="https://tailwindcss.com/">Tailwind CSS</a></li>
-				<li><a href="https://interactjs.io/">interact.js</a></li>
-				<li><a href="https://splidejs.com/">Splide</a></li>
-				<li><a href="https://svooltip.vercel.app/">SVooltip</a></li>
-				<li><a href="https://iconsvg.xyz/">IconSVG</a></li>
-			</ul>
-			<div class="mt-4">
-				Check out the <a href="https://github.com/Suyooo/school-idol-collection">Source Code on GitHub</a>!
-			</div>
-			<div class="mt-4 text-sm text-text-subtle">
-				Love Live!, School Idol Collection, School Idol Festival, character/song/project/group names and icons/images
-				related to the game are copyrighted:<br />
-				©2013 Project Love Live! ©2017 Project Love Live! Sunshine!! ©2019 Project Love Live! Sunshine!! Movie ©KLabGames
-				©SQUARE ENIX CO. ,LTD. ©bushiroad<br />
-				Copyright owners for the card illustrations are listed on a per-card basis on the card pages accessible through the
-				card list.<br />
-				This is a not-for-profit fan project, and unaffiliated with any of the projects or companies above.
-			</div>
+<h1>About</h1>
+<div class="panel">
+	<div class="panel-inner">
+		<div class="">
+			This site and all the translations were made by Suyooo.<br />
+			For questions/problems/mistakes, contact me here:
+		</div>
+		<ul class="ml-4 mt-2 list-inside list-disc">
+			<li>Mastodon / Fediverse: <a href="https://mstdn.schoolidol.club/@Suyooo">@Suyooo@mstdn.schoolidol.club</a></li>
+			<li>Tumblr: <a href="https://suyooo.tumblr.com/">Suyooo</a></li>
+			<li>
+				Or <a href="https://github.com/Suyooo/school-idol-collection/issues">submit it on GitHub</a>
+			</li>
+			<li>
+				Or mail me at <a href="mailto:me@suyo.be">me@suyo.be</a>
+			</li>
+		</ul>
+		<div class="mt-4">Some cool stuff used for this site:</div>
+		<ul class="ml-4 mt-2 list-inside list-disc">
+			<li><a href="https://kit.svelte.dev/">SvelteKit</a></li>
+			<li><a href="https://tailwindcss.com/">Tailwind CSS</a></li>
+			<li><a href="https://interactjs.io/">interact.js</a></li>
+			<li><a href="https://splidejs.com/">Splide</a></li>
+			<li><a href="https://svooltip.vercel.app/">SVooltip</a></li>
+			<li><a href="https://iconsvg.xyz/">IconSVG</a></li>
+		</ul>
+		<div class="mt-4">
+			Check out the <a href="https://github.com/Suyooo/school-idol-collection">Source Code on GitHub</a>!
+		</div>
+		<div class="mt-4 text-sm text-text-subtle">
+			Love Live!, School Idol Collection, School Idol Festival, character/song/project/group names and icons/images
+			related to the game are copyrighted:<br />
+			©2013 Project Love Live! ©2017 Project Love Live! Sunshine!! ©2019 Project Love Live! Sunshine!! Movie ©KLabGames
+			©SQUARE ENIX CO. ,LTD. ©bushiroad<br />
+			Copyright owners for the card illustrations are listed on a per-card basis on the card pages accessible through the
+			card list.<br />
+			This is a not-for-profit fan project, and unaffiliated with any of the projects or companies above.
 		</div>
 	</div>
 </div>
