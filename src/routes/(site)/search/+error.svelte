cacca56c16aa30c08ae031b4324f24111be41215
Suyooo <me@suyo.be>
2023-12-06 22:56:22 +0100

    Fix search input snapshot

--- src/routes/(site)/search/+error.svelte
+++ src/routes/(site)/search/+error.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
-	import SearchTopPage from "./SearchTopPage.svelte";
 	import { page } from "$app/stores";
+	import type { SearchQueryMap } from "$lib/search/types.js";
+	import SearchUi from "./SearchUi.svelte";
+	import PageHeader from "$lib/style/PageHeader.svelte";
+	import type { Snapshot } from "./$types.js";
+
+	let query: SearchQueryMap = {},
+		loadedFromSnapshot = false;
+
+	export const snapshot: Snapshot = {
+		capture: () => query,
+		restore: (value) => {
+			query = value;
+			loadedFromSnapshot = true;
+		},
+	};
 </script>
+
+<svelte:head>
+	<title>Search &bull; SIC</title>
+</svelte:head>
 
 <div class="panel error mb-4">
 	<div class="panel-inner">
@@ -10,4 +28,11 @@
 	</div>
 </div>
 
-<SearchTopPage />
+<PageHeader>Search</PageHeader>
+<div class="panel">
+	<div class="panel-inner">
+		{#key loadedFromSnapshot}
+			<SearchUi bind:query />
+		{/key}
+	</div>
+</div>
--- src/routes/(site)/search/+page.svelte
+++ src/routes/(site)/search/+page.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
-	import SearchTopPage from "./SearchTopPage.svelte";
+	import type { SearchQueryMap } from "$lib/search/types.js";
+	import SearchUi from "./SearchUi.svelte";
+	import PageHeader from "$lib/style/PageHeader.svelte";
+	import type { Snapshot } from "./$types.js";
+
+	let query: SearchQueryMap = {},
+		loadedFromSnapshot = false;
+
+	export const snapshot: Snapshot = {
+		capture: () => query,
+		restore: (value) => {
+			query = value;
+			loadedFromSnapshot = true;
+		},
+	};
 </script>
 
-<SearchTopPage />
+<svelte:head>
+	<title>Search &bull; SIC</title>
+</svelte:head>
+
+<PageHeader>Search</PageHeader>
+<div class="panel">
+	<div class="panel-inner">
+		{#key loadedFromSnapshot}
+			<SearchUi bind:query />
+		{/key}
+	</div>
+</div>
